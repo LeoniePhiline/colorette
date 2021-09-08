e58cb70c418f66ad6543f76c2e446f92feeba45d
Jorge Bucaran <mail@jorgebucaran.com>
2021-09-09 01:46:54 +0900

    Update type definitions

--- index.d.ts
+++ index.d.ts
@@ -1,49 +1,55 @@
 interface Style {
-  (string: string): string
+  (text: string | number): string
 }
 
-export const options: {
-  enabled: boolean
+interface Options {
+  enableColor: boolean
 }
 
-export const reset: Style
-export const bold: Style
-export const dim: Style
-export const italic: Style
-export const underline: Style
-export const inverse: Style
-export const hidden: Style
-export const strikethrough: Style
-export const black: Style
-export const red: Style
-export const green: Style
-export const yellow: Style
-export const blue: Style
-export const magenta: Style
-export const cyan: Style
-export const white: Style
-export const gray: Style
-export const bgBlack: Style
-export const bgRed: Style
-export const bgGreen: Style
-export const bgYellow: Style
-export const bgBlue: Style
-export const bgMagenta: Style
-export const bgCyan: Style
-export const bgWhite: Style
-export const blackBright: Style
-export const redBright: Style
-export const greenBright: Style
-export const yellowBright: Style
-export const blueBright: Style
-export const magentaBright: Style
-export const cyanBright: Style
-export const whiteBright: Style
-export const bgBlackBright: Style
-export const bgRedBright: Style
-export const bgGreenBright: Style
-export const bgYellowBright: Style
-export const bgBlueBright: Style
-export const bgMagentaBright: Style
-export const bgCyanBright: Style
-export const bgWhiteBright: Style
+interface Colors {
+  reset: Style
+  bold: Style
+  dim: Style
+  italic: Style
+  underline: Style
+  inverse: Style
+  hidden: Style
+  strikethrough: Style
+  black: Style
+  red: Style
+  green: Style
+  yellow: Style
+  blue: Style
+  magenta: Style
+  cyan: Style
+  white: Style
+  gray: Style
+  bgBlack: Style
+  bgRed: Style
+  bgGreen: Style
+  bgYellow: Style
+  bgBlue: Style
+  bgMagenta: Style
+  bgCyan: Style
+  bgWhite: Style
+  blackBright: Style
+  redBright: Style
+  greenBright: Style
+  yellowBright: Style
+  blueBright: Style
+  magentaBright: Style
+  cyanBright: Style
+  whiteBright: Style
+  bgBlackBright: Style
+  bgRedBright: Style
+  bgGreenBright: Style
+  bgYellowBright: Style
+  bgBlueBright: Style
+  bgMagentaBright: Style
+  bgCyanBright: Style
+  bgWhiteBright: Style
+}
+
+export const isColorSupported: boolean
+
+export default function (options: Options): Colors
